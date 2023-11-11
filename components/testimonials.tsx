6d9e33b4efa1c7072cf1b08e0383610d78fa38ef
madxjack <pedrofb22@gmail.com>
2023-11-11 13:27:52 +0100

    fixing footer

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -26,11 +26,18 @@ export default function Testimonials() {
         <div className="py-12 md:py-20">
 
           {/* Section header */}
-          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
-            <h2 className="h2 mb-4">Trusted by over 20,000 companies all over the world</h2>
-            <p className="text-xl text-gray-600" data-aos="zoom-y-out">Arcu cursus vitae congue mauris rhoncus viverra nibh cras pulvinar mattis
-              blandit libero cursus mattis.</p>
+          <div className="max-w-3xl mx-auto pb-12 md:pb-16">
+            <h2 className="h2 mb-4">Innovación y Calidad</h2>
+
+            <p className="text-xl text-gray-600" data-aos="zoom-y-out">En Beliz Beauty, combinamos la pasión por la estética con una meticulosa atención al detalle. Cada producto que desarrollamos es una mezcla perfecta de ciencia y arte. Con Beliz StickWax, hemos logrado una fórmula que no solo controla los babyhairs y facilita la creación de estilos únicos, sino que también cuida tu cabello con ingredientes naturales.</p>
           </div>
+
+          <div className="max-w-3xl mx-auto pb-12 md:pb-16">
+            <h2 className="h2 mb-4">Compromiso con la Naturalidad y la Salud del Cabello</h2>
+
+            <p className="text-xl text-gray-600" data-aos="zoom-y-out">Nuestra filosofía se centra en el uso de ingredientes naturales y aromas que cautivan los sentidos. Cada barra de Beliz StickWax está enriquecida con aceites y extractos vegetales seleccionados por sus propiedades beneficiosas para el cabello. Esto asegura que, mientras moldeas tu estilo, tu cabello recibe el cuidado que merece.</p>
+          </div>
+
 
           {/* Items */}
           <div className="max-w-sm md:max-w-4xl mx-auto grid gap-2 grid-cols-4 md:grid-cols-5">
@@ -77,7 +84,7 @@ export default function Testimonials() {
             <div className="relative flex items-start border-2 border-gray-200 rounded bg-white">
 
               {/* Testimonial */}
-              <div className="text-center px-12 py-8 pt-20 mx-4 md:mx-0">
+              <div className="px-1 md:px-12 py-8 pt-20 mx-4 md:mx-0">
                 <div className="absolute top-0 -mt-8 left-1/2 transform -translate-x-1/2">
                   <svg className="absolute top-0 right-0 -mt-3 -mr-8 w-16 h-16 fill-current text-blue-500" viewBox="0 0 64 64" aria-hidden="true" xmlns="http://www.w3.org/2000/svg">
                     <path d="M37.89 58.338c-2.648-5.63-3.572-10.045-2.774-13.249.8-3.203 8.711-13.383 23.737-30.538l2.135.532c-6.552 10.033-10.532 17.87-11.939 23.515-.583 2.34.22 6.158 2.41 11.457l-13.57 8.283zm-26.963-6.56c-2.648-5.63-3.572-10.046-2.773-13.25.799-3.203 8.71-13.382 23.736-30.538l2.136.533c-6.552 10.032-10.532 17.87-11.94 23.515-.583 2.339.22 6.158 2.41 11.456l-13.57 8.283z" />
@@ -85,11 +92,11 @@ export default function Testimonials() {
                   <Image className="relative rounded-full" src={TestimonialImage} width={96} height={96} alt="Testimonial 01" />
                 </div>
                 <blockquote className="text-xl font-medium mb-4">
-                  “ I love this product and would recommend it to anyone. Could be not easier to use, and our multiple websites are wonderful. We get nice comments all the time. “
+                  “ Como profesional del maquillaje y la estética, siempre estoy en busca de productos que no solo realcen la belleza natural de mis clientes, sino que también cuiden su piel y cabello. Desde que descubrí Beliz StickWax, he notado una diferencia significativa en la facilidad de crear peinados sofisticados y duraderos. Lo que más me impresiona es la calidad de los ingredientes naturales, que no solo fijan el cabello, sino que también lo nutren y protegen. Mis clientes están encantados con los resultados y siempre regresan pidiendo más. ¡Beliz Beauty ha revolucionado mi kit de herramientas y es ahora un elemento indispensable en mi trabajo diario!“
                 </blockquote>
-                <cite className="block font-bold text-lg not-italic mb-1">Darya Finger</cite>
-                <div className="text-gray-600">
-                  <span>CEO & Co-Founder</span> <a className="text-blue-600 hover:underline" href="#0">@Dropbox</a>
+                <cite className="block text-center font-bold text-lg not-italic mb-1">Darya Finger</cite>
+                <div className="text-gray-600 text-center">
+                  <span>Maquilladora y Estilista Profesional en </span> <a className="text-blue-600 hover:underline" href="#0">@Noselocrenadie</a>
                 </div>
               </div>
 
