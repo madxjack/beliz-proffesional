54318e3c6be49168b785096264fd178a588508d0
madxjack <pedrofb22@gmail.com>
2023-11-11 15:41:00 +0100

    footer fixed

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function Logo() {
   return (
-    <Link href="/" className="block" aria-label="Cruip">
+    <Link href="/" className="flex justify-center" aria-label="Cruip">
       <img className="h-12 w-auto rounded-full" src="/images/logo.png" alt="Beliz Logo" />
     </Link>
   )
