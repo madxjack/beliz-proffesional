cb9182b06220a02ab1ea578ad2b3b3f7a1211232
Pedro Fernández <pedrofb22@gmail.com>
2024-01-25 16:54:57 +0100

    Traduccion español

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -14,7 +14,7 @@ export default function Header() {
   // detect whether user has scrolled the page down by 10px
   const scrollHandler = () => {
     window.pageYOffset > 10 ? setTop(false) : setTop(true)
-  }  
+  }
 
   useEffect(() => {
     scrollHandler()
@@ -32,20 +32,20 @@ export default function Header() {
             <Logo />
           </div>
 
-          <div className= "font-bold md:text-xl flex-1 md:flex-0 text-center">
+          <div className="font-bold md:text-xl flex-1 md:flex-0 text-center">
             BELIZ BEAUTY
-          </div> 
+          </div>
 
           {/* Desktop navigation */}
           <nav className="hidden md:flex md:grow flex-1 justify-end">
             {/* Desktop sign in links */}
             <ul className="flex flex-wrap items-center">
               <li>
-                <Link href="/signin" className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">Sign in</Link>
+                <Link href="/signin" className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">Iniciar sesión</Link>
               </li>
               <li>
                 <Link href="/signup" className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3">
-                  <span>Sign up</span>
+                  <span>Registrarse</span>
                   <svg className="w-3 h-3 fill-current text-gray-400 shrink-0 ml-2 -mr-1" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                     <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fillRule="nonzero" />
                   </svg>
